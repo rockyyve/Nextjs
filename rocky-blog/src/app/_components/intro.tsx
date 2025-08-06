c766c843348d02ef9353472818fb797fe34c799d
邢岳 <xingyue@myai.tech>
2025-08-06 20:37:30 +0800

    Blog title upd

--- rocky-blog/src/app/_components/intro.tsx
+++ rocky-blog/src/app/_components/intro.tsx
@@ -5,7 +5,7 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Rocky's Blog
       </h1>
       <div className="flex flex-col items-center md:items-end space-y-4">
        
